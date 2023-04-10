40402a9ca0d1d867091f219508a3e9524d935172
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-04-10 15:02:56 +0700

    Initial commit
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   README.md
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   postcss.config.js
    	new file:   public/vite.svg
    	new file:   src/App.vue
    	new file:   src/assets/vue.svg
    	new file:   src/core/Button.vue
    	new file:   src/main.ts
    	new file:   src/router.js
    	new file:   src/style.css
    	new file:   src/vite-env.d.ts
    	new file:   tailwind.config.js
    	new file:   tsconfig.json
    	new file:   tsconfig.node.json
    	new file:   vite.config.ts

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Button :style="'primary'" :size="'lg'" :disabled="true">
+    hello
+  </Button>
+</template>
+
+<script setup lang="ts">
+  import Button from './core/Button.vue';
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/core/Button.vue
@@ -0,0 +1,41 @@
+<template>
+	<button
+		type="button"
+		class="font-semibold shadow-sm"
+		:class="[ BUTTON_STYLE[props.style], BUTTON_SIZE[props.size] ]"
+		@click="handleClick"
+		:disabled="props.disabled"
+		
+	>
+		<slot></slot>
+	</button>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['onClick']);
+
+type ButtonStyle = 'primary' | 'secondary';
+type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+const BUTTON_STYLE: Record<ButtonStyle, string> = {
+	primary: 'bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+	secondary: 'bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+}
+const BUTTON_SIZE: Record<ButtonSize, string> = {
+	xs: 'rounded px-2 py-1 text-xs',
+	sm: 'rounded px-2 py-1 text-sm',
+	md: 'rounded-md px-2.5 py-1.5 text-sm',
+	lg: 'rounded-md px-3 py-2 text-sm',
+	xl: 'rounded-md px-3.5 py-2.5 text-sm'
+}
+
+const props = defineProps({
+	style: { type: String, default: 'primary' },
+	size: { type: String, default: 'xl' },
+	disabled: { type: Boolean, default: false }
+});
+
+const handleClick = () => {
+	emits('onClick');
+};
+</script>
