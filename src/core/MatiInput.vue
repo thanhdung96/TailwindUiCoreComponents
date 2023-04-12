993a70404022b5797373106f6a47ee2648e65d06
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-04-12 10:25:22 +0700

    Added input component.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/core/MatiInput.vue
    	modified:   tailwind.config.js

--- src/core/MatiInput.vue
+++ src/core/MatiInput.vue
@@ -16,6 +16,9 @@
 			:placeholder="props.placeholder"
 			:required="props.required"
 			class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			@change="onChange"
+			@focusin="onFocusIn"
+			@focusout="onFocusOut"
 		/>
 	</div>
 </template>
@@ -28,5 +31,19 @@ const props = defineProps({
 	placeholder: { type: String, default: 'placeholder' },
 	disabled: { type: Boolean, default: false },
 	required: { type: Boolean, default: false },
-})
+});
+
+const emits = defineEmits(['onChange', 'onFocusIn', 'onFocusOut']);
+
+const onChange = () => {
+	emits('onChange');
+};
+
+const onFocusIn = () => {
+	emits('onFocusIn');
+};
+
+const onFocusOut = () => {
+	emits('onFocusOut');
+};
 </script>
