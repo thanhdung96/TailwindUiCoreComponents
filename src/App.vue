5ed166c54fc2c36d4cd7cbbb57f71c01b202f051
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-04-14 15:49:00 +0700

    Adding interface.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	modified:   src/core/MatiNav.vue
    	new file:   src/core/interfaces/INavItem.ts
    	modified:   tsconfig.json

--- src/App.vue
+++ src/App.vue
@@ -245,20 +245,16 @@ import {
 	XMarkIcon,
 } from '@heroicons/vue/24/outline'
 import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import { NavItemListType, NavItemType } from '@/core/interfaces/INavItem'
 
-const navigation = [
-	{ name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-	{ name: 'Team', href: '#', icon: UsersIcon, current: false },
-	{ name: 'Projects', href: '#', icon: FolderIcon, current: false },
-	{ name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-	{
-		name: 'Documents',
-		href: '#',
-		icon: DocumentDuplicateIcon,
-		current: false,
-	},
-	{ name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
+const navigation = new NavItemListType([
+	new NavItemType('#', true, 'Dashboard', HomeIcon),
+	new NavItemType('#', true, 'Team', UsersIcon),
+	new NavItemType('#', true, 'Projects', FolderIcon),
+	new NavItemType('#', true, 'Calendar', CalendarIcon),
+	new NavItemType('#', true, 'Documents', DocumentDuplicateIcon),
+	new NavItemType('#', true, 'Reports', ChartPieIcon),
+])
 
 const userNavigation = [
 	{ name: 'Your profile', href: '#' },
--- src/core/MatiNav.vue
+++ src/core/MatiNav.vue
@@ -4,17 +4,17 @@
 			<li>
 				<ul role="list" class="-mx-2 space-y-1">
 					<MatiNavItem
-						v-for="item in props.navItemList"
+						v-for="item in props.navItemList.lstNavItem"
 						:key="item.name"
 						:nav-item="item"
 					/>
 				</ul>
 			</li>
 
-			<li v-if="subNavItemList.length > 0" class="mt-auto">
+			<li v-if="subNavItemList.lstNavItem.length > 0" class="mt-auto">
 				<ul role="list" class="-mx-2 space-y-1">
 					<MatiNavItem
-						v-for="subItem in props.subNavItemList"
+						v-for="subItem in props.subNavItemList.lstNavItem"
 						:key="subItem.name"
 						:nav-item="subItem"
 					/>
@@ -25,11 +25,17 @@
 </template>
 
 <script setup lang="ts">
+// import { PropType } from 'vue'
 import MatiNavItem from './MatiNavItem.vue'
+import { NavItemListType } from '@/core/interfaces/INavItem'
 
 const props = defineProps({
-	navItemList: { type: Array, required: true, default: () => [] },
-	subNavItemList: { type: Array, required: false, default: () => [] },
+	navItemList: { type: NavItemListType, required: true, default: () => [] },
+	subNavItemList: {
+		type: NavItemListType,
+		required: false,
+		default: () => [],
+	},
 	withIcon: { type: Boolean, default: true },
 })
 </script>
